6f033eb60240819e4382034f6078e71bd1358b57
Tim Shearouse <ShearouseTimothyW@JohnDeere.com>
2015-11-09 13:04:58 -0600

    Split the list of products on the catalog into lists of the respective product sub-classes
    Signed-off-by: Tim Shearouse <ShearouseTimothyW@JohnDeere.com>

--- source/ADAPT/Catalog.cs
+++ source/ADAPT/Catalog.cs
@@ -39,7 +39,10 @@ namespace AgGateway.ADAPT.ApplicationDataModel
         public string Name { get; set; }
         public List<Person> Persons { get; set; } 
         public List<PersonRole> PersonRoles { get; set; }
-        public List<Product> Products { get; set; } 
+        public List<CropVariety> CropVarieties { get; set; }
+        public List<CropProtectionProduct> CropProtectionProducts { get; set; }
+        public List<FertilizerProduct> FertilizerProducts { get; set; }
+        public List<ProductMix> ProductMixes { get; set; }
         public List<ProductMixComponent> ProductMixComponents { get; set; }
         public List<TimeScope> TimeScopes { get; set; }
         public List<Machine> Machines { get; set; }
