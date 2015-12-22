97171f6b63a6542780e69aad11c3d5e6d2de1894
Kathleen Oneal <onealkathleena@johndeere.com>
2015-12-22 16:02:53 -0600

    Added values to CategoryEnum -- Signed-off-by-Kathleen Oneal <onealkathleena@johndeere.com>

--- source/ADAPT/CategoryEnum.cs
+++ source/ADAPT/CategoryEnum.cs
@@ -9,11 +9,15 @@
   * Contributors:
   *    Tarak Reddy, Tim Shearouse - initial API and implementation
   *    Joseph Ross - Renaming Enums to end in Enum
+  *    Kathleen Oneal - added values
   *******************************************************************************/
 
 namespace AgGateway.ADAPT.ApplicationDataModel
 {
     public enum CategoryEnum
     {
+        Fungicide,
+        Herbicide,
+        Manure,
     }
 }
