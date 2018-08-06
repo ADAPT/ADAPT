97720164ed05e8f77633f79b065c8fc8d21b4f0b
Luis <luis@pct-ag.com>
2018-08-06 16:04:26 -0700

    https://github.com/ADAPT/ADAPT/issues/170 Representation Deserializations - Invalid Path to XML Resources With ASP.NET Projects

--- source/Representation/RepresentationSystem/RepresentationManager.cs
+++ source/Representation/RepresentationSystem/RepresentationManager.cs
@@ -56,7 +56,7 @@ namespace AgGateway.ADAPT.Representation.RepresentationSystem
         {
             var serializer = new XmlSerializer(typeof(Generated.RepresentationSystem));
 
-            var assemblyLocation = AppDomain.CurrentDomain.BaseDirectory;
+            var assemblyLocation = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
             var repSystemXml = Path.Combine(assemblyLocation, "Resources", "RepresentationSystem.xml");
 
             var xmlStringBytes = File.ReadAllBytes(repSystemXml);
