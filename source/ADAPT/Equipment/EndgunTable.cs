f6cb8177258f2795f78c193df91fba3bfc868d1e
Andres Ferreyra <andres.ferreyra@agconnections.com>
2018-09-25 17:27:16 -0500

    EndgunTable and EndgunTableEntry
    These two classes enable the description of two behaviors of an endgun: its throw vs pressure, and flow vs pressure functions. Used in EndgunConfiguration.

---
+++ source/ADAPT/Equipment/EndgunTable.cs
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (C) 2016 AgGateway and ADAPT Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html <http://www.eclipse.org/legal/epl-v10.html> 
+ *
+ * Contributors: Aaron Berger: Transcribed from PAIL
+ *    
+ *******************************************************************************/
+
+namespace AgGateway.ADAPT.ApplicationDataModel.Equipment
+{
+    class EndgunTable
+    {
+        public EndgunTable()
+        {
+            TableEntry = new List<EndgunTableEntry>();
+        }
+        public List<EndgunTableEntry> TableEntry { get; set; }
+    }
+}
