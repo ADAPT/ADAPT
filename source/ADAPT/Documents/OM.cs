47668b56dc9adda233b4593d71d82df930b56750
Andres Ferreyra <andres.ferreyra@agconnections.com>
2019-04-30 21:42:30 -0500

    Update OM.cs

--- source/ADAPT/Documents/OM.cs
+++ source/ADAPT/Documents/OM.cs
@@ -11,6 +11,7 @@
 
 using System.Collections.Generic;
 using AgGateway.ADAPT.ApplicationDataModel.Common;
+using AgGateway.ADAPT.ApplicationDataModel.Shapes;
 
 namespace AgGateway.ADAPT.ApplicationDataModel.Documents
 {
