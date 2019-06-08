c14602c0c3bb7791f5cc9621641a5955a1942ed1
Frank <frank.wiebeler@dev4agriculture.de>
2019-06-08 08:57:22 +0200

    Removed useless empty line

--- source/ADAPT/Equipment/ConnectorTypeEnum.cs
+++ source/ADAPT/Equipment/ConnectorTypeEnum.cs
@@ -24,7 +24,6 @@ namespace AgGateway.ADAPT.ApplicationDataModel.Equipment
         ISO64894PitonTypeCoupling,
         ISO56922PivotWagonHitch,
         ISO24347BallTypeHitch,
-
-        ChassisMountedSelfPropelled
+        ChassisMountedSelfPropelled,
     }
 }
