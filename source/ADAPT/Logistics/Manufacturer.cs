cf7771bebd44d909f1522772ff2177e6fc3d9ead
Andres Ferreyra <andres.ferreyra@agconnections.com>
2020-05-27 08:47:53 -0500

    Adding ContextItems to Manufacturer

--- source/ADAPT/Logistics/Manufacturer.cs
+++ source/ADAPT/Logistics/Manufacturer.cs
@@ -1,6 +1,7 @@
 /*******************************************************************************
   * Copyright (C) 2015 AgGateway and ADAPT Contributors
   * Copyright (C) 2015 Deere and Company
+  * Copyright (C) 2020 SYngenta Crop Protection LLC
   * All rights reserved. This program and the accompanying materials
   * are made available under the terms of the Eclipse Public License v1.0
   * which accompanies this distribution, and is available at
@@ -10,8 +11,11 @@
   *    Tarak Reddy, Tim Shearouse - initial API and implementation
   *    Joseph Ross Making Properties
   *    Justin Sliekers - dropping context items
+  *    Andres Ferreyra - 20200527 re-adding ContextItems; e.g., to support GLNs for ISO 11783 Annex E manufacturer Ids.  
   *******************************************************************************/
 
+using System;
+using System.Collections.Generic;
 using AgGateway.ADAPT.ApplicationDataModel.Common;
 
 namespace AgGateway.ADAPT.ApplicationDataModel.Logistics
@@ -21,10 +25,13 @@ namespace AgGateway.ADAPT.ApplicationDataModel.Logistics
         public Manufacturer()
         {
             Id = CompoundIdentifierFactory.Instance.Create();
+            ContextItems = new List<ContextItem>();            
         }
 
         public CompoundIdentifier Id { get; private set; }
 
         public string Description { get; set; }
+        
+        public List<ContextItem> ContextItems { get; set; }        
     }
 }
