41a86bba7212b72674be4216f629f8b7eb8564a3
Andres Ferreyra <andres.ferreyra@agconnections.com>
2020-08-18 21:45:34 -0500

    Add to PackagedProduct; add PackagedProductInstance, Container

--- source/ADAPT/Products/Container.cs
+++ source/ADAPT/Products/Container.cs
@@ -7,23 +7,20 @@
   * http://www.eclipse.org/legal/epl-v10.html <http://www.eclipse.org/legal/epl-v10.html> 
   *
   * Contributors:
-  *    R. Andres Ferreyra - initial implementation based on ProductStatusEnum class.
-  *    R. Andres Ferreyra - added IsBulk attribute 8/18/20.
+  *    R. Andres Ferreyra - initial implementation based on SPADE3, Traceability WG.
   *  *******************************************************************************/
 
 using System.Collections.Generic;
 using AgGateway.ADAPT.ApplicationDataModel.Common;
-using AgGateway.ADAPT.ApplicationDataModel.Representations;
 
 namespace AgGateway.ADAPT.ApplicationDataModel.Products
 {
-    public class PackagedProduct
+    public class Container
     {
 
-        public PackagedProduct()
+        public Container()
         {
             Id = CompoundIdentifierFactory.Instance.Create();
-            ContainedPackagedProducts = new List<ContainedPackagedProduct>();
             ContextItems = new List<ContextItem>();
         }
 
@@ -31,21 +28,11 @@ namespace AgGateway.ADAPT.ApplicationDataModel.Products
 
         public string Description { get; set; }
 
-        public int ProductId { get; set; }
+        public int? ContainerModelId { get; set; }
 
-        public int ContainerModelId { get; set; }
-
-        public bool IsBulk { get; set; }
-
-        public PackagedProductStatusEnum Status { get; set; }
+        public ContainerUseTypeEnum ContainerUseType { get; set; }
         
-        public NumericRepresentationValue ProductQuantity { get; set; }
-
-        public List<ContainedPackagedProduct> ContainedPackagedProducts { get; set; }
-
-        public NumericRepresentationValue GrossWeight { get; set; }
-
-        public NumericRepresentationValue NetWeight { get; set; }
+        public int? CorrespondingDeviceElementId { get; set; }
 
         public List<ContextItem> ContextItems { get; set; } 
 
