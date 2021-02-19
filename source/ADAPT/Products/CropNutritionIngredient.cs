26def3f0178d65ea0381a21865c5d5a8742aab1d
Kelly Nelson <knelson@farmbeltnorth.com>
2021-02-19 13:01:24 -0600

    Extending ProductComponent to include a mapping to a Product, adding an InertIngredient, and adding comments to product mix classes.

--- source/ADAPT/Products/CropNutritionIngredient.cs
+++ source/ADAPT/Products/CropNutritionIngredient.cs
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
   * Copyright (C) 2015 AgGateway and ADAPT Contributors
   * Copyright (C) 2015 Deere and Company
   * All rights reserved. This program and the accompanying materials
@@ -9,14 +9,21 @@
   * Contributors:
   *    Kathleen Oneal - initial API and implementation
   *    Stuart Rhea - #112 CropNutritionIngredientCodeEnum needs to be changed into an EnumeratedRepresentation and have the oxides added to the list
+  *    Kelly Nelson - added XML Comments
   *******************************************************************************/
 
 using AgGateway.ADAPT.ApplicationDataModel.Representations;
 
 namespace AgGateway.ADAPT.ApplicationDataModel.Products
 {
+    /// <summary>
+    /// An crop nutrition element of a Product, mapped via ProductComponent
+    /// </summary>
     public class CropNutritionIngredient : Ingredient
     {
+        /// <summary>
+        /// A reference to the element, using for example dtCropNutritionIngredient
+        /// </summary>
         public EnumeratedValue IngredientCode { get; set; }
     }
 }
--- source/ADAPT/Products/ProductComponent.cs
+++ source/ADAPT/Products/ProductComponent.cs
@@ -1,4 +1,4 @@
-﻿/*******************************************************************************
+/*******************************************************************************
   * Copyright (C) 2015 AgGateway and ADAPT Contributors
   * Copyright (C) 2015 Deere and Company
   * Copyright (C) 2019 Syngenta
@@ -10,23 +10,50 @@
   * Contributors:
   *    Tim Shearouse - initial API and implementation
   *    R. Andres Ferreyra - Added MixOrder, as requested for Mix Ticket "Product Group" compatibility.
+  *    Kelly Nelson - Added ProductId and XML comments on members
   *******************************************************************************/
 
 using AgGateway.ADAPT.ApplicationDataModel.Representations;
 
 namespace AgGateway.ADAPT.ApplicationDataModel.Products
 {
+    /// <summary>
+    /// An individual component that comprises part of a Product
+    /// </summary>
     public class ProductComponent
     {
+        /// <summary>
+        /// A reference to another independent product, used as a component in this product
+        /// Either ProductId or IngredientId may be set, but not both
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// A reference to a crop nutrition element, active chemical ingredient or inert ingredient of this product
+        /// Either ProductId or IngredientId may be set, but not both
+        /// </summary>
         public int IngredientId { get; set; }
 
+        /// <summary>
+        /// Quantity may be set in any appropriate Representation.
+        /// E.g.s, vrProductContent, vrSolutionRateLiquid, vrActiveIngredientMassPerMass, etc.
+        /// </summary>
         public NumericRepresentationValue Quantity { get; set; }
 
+        /// <summary>
+        /// True if the component refers to a Product, false if refers to an Ingredient
+        /// </summary>
         public bool IsProduct { get; set; }
 
+        /// <summary>
+        /// True if this component represents the primary liquid contents of a mix
+        /// </summary>
         public bool IsCarrier { get; set; }
-        
-        public int? MixOrder { get; set; } // In what order was this component added to the mix? (1 = First).
-        // Can have duplicate numbers, representing a situation where two or more components are added simultaneously.
+
+        /// <summary>
+        /// In what order was this component added to the mix? (1 = First).
+        /// Can have duplicate numbers, representing a situation where two or more components are added simultaneously.
+        /// </summary>
+        public int? MixOrder { get; set; } 
     }
 }
