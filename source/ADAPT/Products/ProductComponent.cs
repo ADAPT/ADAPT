dc5faf2c55b6fa4ac9426ac81ab5f038f73a5b74
Kelly Nelson <knelson@farmbeltnorth.com>
2021-02-27 09:44:31 -0600

    Modifying change to explain current implementation without adding a new property on ProductComponent.

--- source/ADAPT/Products/ProductComponent.cs
+++ source/ADAPT/Products/ProductComponent.cs
@@ -10,7 +10,7 @@
   * Contributors:
   *    Tim Shearouse - initial API and implementation
   *    R. Andres Ferreyra - Added MixOrder, as requested for Mix Ticket "Product Group" compatibility.
-  *    Kelly Nelson - Added ProductId and XML comments on members
+  *    Kelly Nelson - Added XML comments on members
   *******************************************************************************/
 
 using AgGateway.ADAPT.ApplicationDataModel.Representations;
@@ -23,14 +23,12 @@ namespace AgGateway.ADAPT.ApplicationDataModel.Products
     public class ProductComponent
     {
         /// <summary>
-        /// A reference to another independent product, used as a component in this product
-        /// Either ProductId or IngredientId may be set, but not both
-        /// </summary>
-        public int ProductId { get; set; }
-
-        /// <summary>
-        /// A reference to a crop nutrition element, active chemical ingredient or inert ingredient of this product
-        /// Either ProductId or IngredientId may be set, but not both
+        /// IngredientId may refer to an Catalog Ingredient (crop nutrition element, active or inert chemical ingredient)
+        /// or a Catalog Product (where the product itself is part of the mix, and it may have its own nested ingredients)
+        /// While potentially confusing, this dual-meaning reference property was chosen early in the ADAPT project over the
+        /// the option of having separate IngredientId and ProductId properties witout the ability to enforce only
+        /// one or the other may be set.
+        /// The IsProduct property below governs the type of object to which IngredientId refers
         /// </summary>
         public int IngredientId { get; set; }
 
