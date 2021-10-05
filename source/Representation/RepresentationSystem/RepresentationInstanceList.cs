726643584fe51db2775b3cea531f14921dfd915e
Jason Roesbeke <30174346+JasonRoesbeke@users.noreply.github.com>
2021-10-05 15:37:53 +0200

    removing header

--- source/Representation/RepresentationSystem/RepresentationInstanceList.cs
+++ source/Representation/RepresentationSystem/RepresentationInstanceList.cs
@@ -1,16 +1,3 @@
-/*******************************************************************************
-  * Copyright (C) 2015-2016 AgGateway and ADAPT Contributors
-  * Copyright (C) 2015-2016 Deere and Company
-  * All rights reserved. This program and the accompanying materials
-  * are made available under the terms of the Eclipse Public License v1.0
-  * which accompanies this distribution, and is available at
-  * http://www.eclipse.org/legal/epl-v10.html <http://www.eclipse.org/legal/epl-v10.html> 
-  *
-  * Contributors:
-  *    Tarak Reddy, Tim Shearouse - initial API and implementation
-  *******************************************************************************/
-
-
 namespace AgGateway.ADAPT.Representation.RepresentationSystem
 {
     public class RepresentationInstanceList 
